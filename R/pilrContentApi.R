b860e53a3128e3fdff4c4bd3b09060c3e572a75f
Mark Mendel <mmendel@meinergy.com>
2019-06-25 11:54:58 -0500

    code cleanup

--- R/pilrContentApi.R
+++ R/pilrContentApi.R
@@ -5,6 +5,8 @@
 ##################################################################################
 
 library('mirtCAT')
+
+#' Set global variable df, the mirtCAT data-frame that defines the survey.
 data(CATDesign)
 
 #' A wrapper around mirtCAT#updateDesign()
@@ -21,18 +23,25 @@ compute.fn <- function(design.elements, questions, answers) {
   })
 }
 
-
+#' Wrap PiLR Content API around a mirtCAT survey
+#' 
+#' See "232 - KU Cat Design - Remote Calculation Card" (https://docs.google.com/document/d/1fC8kag54Ttm9Yy0vm3oayHKyk5jLnvHw9e5MOqrkZJo)
+#'
+#' @param participantCode not used here but part of API
+#' 
+#' @param resultsSoFar the 'result' variable from EMA survey director.  
+#'   It defines the questions asked so far and the answers given
+#'   
+#' @param sourceCard the calulated content card object that triggered this request. 
+#' 
+#' @return a list of cards objects with which EMA replaces the sourceCard in the survey
+#' 
 #' @export
 #' @import mirtCAT
 pilrContentApi <- function(participantCode, resultsSoFar, sourceCard,
                              # following parameters are test hooks.
                              computeFn = compute.fn,
                              mirtCatDataFrame = df) {
-  design.elements <- mirtCAT(df, mod, criteria = 'KL', start_item = 'Trule',
-                             design_elements = TRUE,
-                             design = list(min_SEM = rep(0.4, 3),
-                                           max_items = ncol(data_epsi1a),
-                                           delta_thetas = rep(0.03, 3)))
   questions <- c()
   answers <- c()
   for (section in resultsSoFar) {
@@ -42,12 +51,22 @@ pilrContentApi <- function(participantCode, resultsSoFar, sourceCard,
     questions <- c(questions, as.numeric(qs))
     answers <- c(answers, as.numeric(as))
   }
+  
+  design.elements <- mirtCAT(df, mod, criteria = 'KL', start_item = 'Trule',
+                             design_elements = TRUE,
+                             design = list(min_SEM = rep(0.4, 3),
+                                           max_items = ncol(data_epsi1a),
+                                           delta_thetas = rep(0.03, 3)))
+  
   nextQuestionIx <- computeFn(design.elements, questions, answers)
 
+  # Convert mirtCAT options [dataframe columns named 'Option-0', etc] to card options
   option.names = names(mirtCatDataFrame)[grepl('Option.*', names(mirtCatDataFrame))]
   options <- lapply(mirtCatDataFrame[nextQuestionIx, option.names], function(optStr) {
     parts <- strsplit(optStr, '-')[[1]]
-    list(value = parts[[1]], text = parts[[2]], order=1+as.numeric(parts[[1]]))
+    list(value = parts[[1]], 
+         text = parts[[2]], 
+         order=1+as.numeric(parts[[1]]))
   })
   names(options) <- NULL
   
@@ -58,8 +77,11 @@ pilrContentApi <- function(participantCode, resultsSoFar, sourceCard,
                                      text = '',
                                      code = paste0('mc:', nextQuestionIx),
                                      options = options))
-  sourceCard$section <- sourceCard$section + 1
-  list(result=list(calculatedCard, sourceCard))
+  
+  nextCalcCard <- sourceCard
+  nextCalcCard$section <- nextCalcCard$section + 1
+  
+  list(result=list(calculatedCard, nextCalcCard))
 }
 
 # dump inputs passed by openCPU for use in sample-parameters.R for testing
