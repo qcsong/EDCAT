1d81a93330e795e978db83ecbf652da6f1ad620a
Mark Mendel <mmendel@meinergy.com>
2019-07-02 09:10:32 -0500

    Added design = list(min_SEM_0.5)) per Chelsea's request

--- R/findNextQuestionIx.R
+++ R/findNextQuestionIx.R
@@ -1,5 +1,5 @@
 #' A wrapper around mirtCAT#updateDesign() 
-findNextQuestionIx <- function(design.elements, questions, answers) {
+findNextQuestionIx <- function(design.elements, questions, answers, design=list(min_SEM_0.5)) {
   if (is.null(questions)) {
     return(1)
   }
