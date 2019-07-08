dc6251463c53e15218fb7cf1ba591f50b66cc660
Mark Mendel <mmendel@meinergy.com>
2019-07-08 16:48:22 -0500

    don't mask errors from findNext

--- R/findNextQuestionIx.R
+++ R/findNextQuestionIx.R
@@ -4,10 +4,11 @@ findNextQuestionIx <- function(design.elements, questions, answers, design=list(
     return(1)
   }
   updateDesign(design.elements, items=questions, responses=answers)
-  tryCatch({
-    findNextItem(design.elements)
-  },
-  error = function(error_condition) {
-    1
-  })
+  findNextItem(design.elements)
+  # tryCatch({
+  #   findNextItem(design.elements)
+  # },
+  # error = function(error_condition) {
+  #   NA
+  # })
 }
