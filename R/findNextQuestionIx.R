63b693bf24427df28c740e31cc8001ed915dc9cb
Mark Mendel <mmendel@meinergy.com>
2019-07-18 09:36:21 -0500

    Update docs. Extract buildMirtCatStateObject() function to aid in tuning mirtCAT parameters.

--- R/findNextQuestionIx.R
+++ R/findNextQuestionIx.R
@@ -1,6 +1,21 @@
 #' A facade for mirtCAT used by pilrContentApi()
 #' 
-#' # Data Files
+#' findNextQuestionIx() is the function used by pilrContentApi().
+#' 
+#' buildMirtCatStateObject() is an internal function that is useful for tuning the survey. It returns the CATDesign parmeter required by 
+#' mirtCAT::findNextItem().   
+#' 
+#' TUNING EXAMPLE
+#'
+#' Simulate asking questions 1, 40 and 3 with responses if alaws 2.  Examine resulting state's theta history
+#'  
+#' > x <- buildMirtCatStateObject(questions=c(1, 40, 3), answers=c(2, 2, 2))
+#' > x$person$thetas_history
+#' F1            F2        F3
+#' [1,] 0.0000000  0.000000e+00 0.0000000
+#' [2,] 0.1611487 -3.615848e-07 0.9306835
+#'
+#' DATA FILES
 #' 
 #' The data/ files were extracted from MultiCATinput_EPSI_09-21-18.RData from the demo
 #' Shiny app as follows:
@@ -29,6 +44,7 @@ mirtCAT.preCAT = list(min_items = 15,
 
 #' Calculate next question to ask
 #' 
+#' 
 #' @param questions is a vector of the indicies of the questions answered so far
 #' 
 #' @param answers is a vector of the responses to corresponding questions
@@ -37,22 +53,36 @@ mirtCAT.preCAT = list(min_items = 15,
 #' 
 #' @import mirtCAT
 #' @export
-findNextQuestionIx <- function(questions, answers, design=list(min_SEM=0.5)) {
-  if (is.null(questions)) {
-    return(1)
-  }
-  
-  # need to build fresh every time because update.disgn modifies it
-  CATdesign <- mirtCAT(mirtCAT.df, mirtCAT.mo, 
-                             preCAT = mirtCAT.preCAT,
-                             design_elements = TRUE)
-  updateDesign(CATdesign, items=questions, responses=answers)
-  CATdesign$design@Update.thetas(CATdesign$design, CATdesign$person, CATdesign$test)
+findNextQuestionIx <- function(questions, answers) {
   tryCatch({
+    CATdesign <- buildMirtCatStateObject(questions, answers)
     findNextItem(CATdesign)
   }, 
-  # will get error if there are no more questions. Treat as if it terminated cleanly
-  error = function(err) { NA })
+  error = function(err) { 
+    # will get error if there are no more questions. Treat as if it terminated cleanly
+    NA 
+  })
+}
+
+#' Construct the object that enapsulate the state of mirtCAT survey from responses so far
+#' 
+#' @param questions is a vector of the indicies of the questions that have been answered
+#' 
+#' @paaram answers is a vector of the indicies of the responses
+#' 
+#' @return the state object x such that mirtCAT::findNextItem(x) returns the next question to be asked.
+#' 
+buildMirtCatStateObject <- function(questions, answers) {
+  CATdesign <- mirtCAT(mirtCAT.df, mirtCAT.mo, 
+                       preCAT = mirtCAT.preCAT,
+                       design = list(min_SEM=0.5),
+                       design_elements = TRUE)
+  if (is.null(questions)) {
+    return(CATdesign)
+  }
+  CATdesign <- updateDesign(CATdesign, items=questions, responses=answers)
+  CATdesign$design@Update.thetas(CATdesign$design, CATdesign$person, CATdesign$test)
+  CATdesign
 }
 
 titleForQuestion <- function(questionIx) {
